a4ae68d71a849fb1ea9aa59662e14a7095645a04
Chernyshov_Nikita <xpom5566@mail.ru>
2020-04-08 21:44:24 +0300

    solved: 96ms (96.74% !!!) / 18.1 (6.3%)

--- main.py
+++ main.py
@@ -1,4 +1,50 @@
+from typing import List
+
+
+class Solution:
+
+  def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    obj = {}
+
+    # Count all nums
+    for item in nums:
+      if not item in obj:
+        obj[item] = 1
+      else:
+        obj[item] += 1
+
+    # obj = {
+    #   1: 3
+    #   2: 2
+    #   3: 1
+    # }
+
+    # Make Array<Object> structure
+    keys = list(map(lambda item: {'key': item, 'value': obj[item] }, list(obj.keys())))
+
+    # keys = [{
+    #   key: 2,
+    #   value: 2
+    # }, {
+    #   key: 1,
+    #   value; 3
+    # }, {
+    #   key: 3,
+    #   value: 1
+    # }]
+
+    # Sort by value key (item['value'])
+    keys.sort(key=lambda item: item['value'], reverse=True)
+
+    return [keys[index]['key'] for index in range(k)]
+
+
 my = Solution()
-n = 0
-ans = my.(n)
+n0 = [1, 1, 1, 2, 2, 3]
+k0 = 2
+n1 = [4,1,-1,2,-1,2,3]
+k1 = 2
+
+ans = my.topKFrequent(n0, k0)
+# ans = my.topKFrequent(n1, k1)
 print("ans", ans)
